f7ddca07f949a6c89edfdd0659ba4671fcbed4a3
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-19 18:44:13 +0200

    add component scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="text-3xl font-bold">TODO APP</div>
+      <div className="text-3xl font-bold drop-shadow-lg">TODO APP</div>
     </div>
   );
 };
 
-const TodoList = () => {
+type TodoListProps = {
+  children: ReactNode | ReactNode[];
+};
+
+const TodoList = ({ children }: TodoListProps) => {
   return (
     <div className="todo-list-container">
-      <div>Todos</div>
+      <div>{children}</div>
     </div>
   );
+};
+
+enum TodoStatus {
+  Active = "Activec",
+  Done = "Done",
+  Archived = "Archived",
+}
+
+type TodoItemType = {
+  title: string;
+  description: string;
+  id: string;
+  status: TodoStatus;
+};
+
+type TodoItemProps = {
+  item: TodoItemType;
+};
+
+const TodoItem = ({ item }: TodoItemProps) => {
+  const { title, description, status } = item;
+  return (
+    <div className="todo-item-container">
+      {title} - {description} - {status}
+    </div>
+  );
+};
+
+const todoItem: TodoItemType = {
+  title: "Clean the kitchen",
+  description: "Clean that motherfucker",
+  id: "lkjadlkj0192i3",
+  status: TodoStatus.Archived,
 };
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
-      <TodoList />
+      <TodoList>
+        <TodoItem item={todoItem} />
+      </TodoList>
     </div>
   );
 };
