594318eb00296e21b1dd589bc446681e81f0a03e
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-20 00:18:53 +0200

    add fetching items from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
+import { FadeLoader } from "react-spinners";
 
 // components
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 import TodoList from "./components/TodoList";
+import { getTodoItemsByUserId } from "./services/firebaseLogic";
 
 // types
 import { TodoItemType } from "./types/todoTypes";
@@ -25,19 +27,33 @@ const ContentContainer = ({ children }: ContentContainerProps) => {
 
 const App = () => {
   const [todoItems, setTodoItems] = useState<TodoItemType[] | undefined>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const items = await getTodoItemsByUserId("ada");
+
+      console.log(items);
+      setTodoItems(items);
+      setLoading(false);
+    };
+
+    getData();
+  }, []);
 
   return (
     <div className="app-container">
       <Header />
       <ContentContainer>
-        <TodoList>
-          <TodoItem item={todoItem} />
-          <TodoItem item={todoItem} />
-          <TodoItem item={todoItem} />
-          <TodoItem item={todoItem} />
-          <TodoItem item={todoItem} />
-          <TodoItem item={todoItem} />
-        </TodoList>
+        {loading ? (
+          <FadeLoader />
+        ) : (
+          <TodoList>
+            {todoItems?.map((item) => (
+              <TodoItem item={item} />
+            ))}
+          </TodoList>
+        )}
       </ContentContainer>
     </div>
   );
