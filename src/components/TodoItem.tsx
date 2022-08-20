c854a0ec09f5b7791d033463332808ff01034179
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-20 16:11:31 +0200

    add styling to todoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BsCheckLg } from "react-icons/bs";
 
 // types
 import { TodoItemType } from "../types/todoTypes";
+
+type CheckmarkProps = {
+  checked: boolean;
+};
+
+const Checkmark = ({ checked }: CheckmarkProps) => {
+  if (checked) {
+    return (
+      <div className="w-6 h-6 flex justify-center items-center bg-theme-light rounded-md border-2 border-theme-primary shadow">
+        <BsCheckLg className="text-theme-primary mt-[2px]" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-6 h-6 rounded-md border-2 border-theme-primary shadow"></div>
+  );
+};
 
 type TodoItemProps = {
   item: TodoItemType;
@@ -12,10 +31,14 @@ const TodoItem = ({ item, handleToggle }: TodoItemProps) => {
   const { title, done } = item;
   return (
     <button className="todo-item-container" onClick={() => handleToggle(item)}>
-      <div className="w-6 h-6 rounded-md border-2 border-theme-primary shadow"></div>
-      <div className="font-medium text-lg">
-        {title} - {done ? "Done" : "Not done"}
-      </div>
+      <Checkmark checked={done} />
+      {done ? (
+        <div className="font-medium text-lg text-slate-400 line-through">
+          {title}
+        </div>
+      ) : (
+        <div className="font-medium text-lg">{title}</div>
+      )}
     </button>
   );
 };
