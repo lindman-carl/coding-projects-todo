6026fe55beb6cee47aabdc4666d27d78da9880a1
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-24 23:24:02 +0200

    add options to todoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsCheckLg, BsThreeDots } from "react-icons/bs";
 
 // types
@@ -28,24 +28,34 @@ type TodoItemProps = {
 };
 
 const TodoItem = ({ item, handleToggle }: TodoItemProps) => {
+  const [optionsExpanded, setOptionsExpanded] = useState<boolean>(false);
+
   const { title, done } = item;
   return (
-    <button className="todo-item-container" onClick={() => handleToggle(item)}>
-      <Checkmark checked={done} />
-      {done ? (
-        <div className="font-medium text-md text-slate-400 line-through">
-          {title}
-        </div>
-      ) : (
-        <div className="font-medium text-md">{title}</div>
-      )}
+    <div className="todo-item-container">
       <button
-        className="text-theme-primary ml-auto active:scale-125 sm:hover:scale-125 transition-transform"
-        onClick={() => console.log("clicked dots")}
+        onClick={() => handleToggle(item)}
+        className="flex flex-row flex-grow justify-start gap-x-8 active:underline"
       >
-        <BsThreeDots />
+        <Checkmark checked={done} />
+        {done ? (
+          <div className="font-medium text-md text-slate-400 line-through">
+            {title}
+          </div>
+        ) : (
+          <div className="font-medium text-md">{title}</div>
+        )}
       </button>
-    </button>
+      <div className="inline-flex">
+        <button
+          className="text-theme-primary ml-auto active:scale-125 sm:hover:scale-125 transition-transform"
+          onClick={() => setOptionsExpanded((prev) => !prev)}
+        >
+          <BsThreeDots className={optionsExpanded ? "rotate-90" : ""} />
+        </button>
+        {optionsExpanded && <div className="px-2">Edit | Delete</div>}
+      </div>
+    </div>
   );
 };
 
